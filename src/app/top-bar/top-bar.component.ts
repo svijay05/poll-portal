1f14e9fe8373bf57a89ef4aed5474b78d459abbc
Sreenath Vijayakumaran <s4sreenath@gmail.com>
2020-01-29 00:00:37 -0500

    page navigation method swiching from a href to angular router based for clean url

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'top-bar',
@@ -7,10 +8,19 @@ import { Component,OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-   title:string = "Poll Portal" ;
+  title: string = "Poll Portal";
+  constructor(private router: Router) {
 
-   ngOnInit(){
+  }
+
+  ngOnInit() {
     console.log("top-bar component init")
-   }
+  }
 
+  navHome() {
+    this.router.navigateByUrl('/');
+  }
+  navCreate() {
+    this.router.navigateByUrl('/questions/create');
+  }
 }
